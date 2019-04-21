29d8c06991e7e28a9aa3063171df93058c9e5dfa
Zane Rockenbaugh <zane@liquid-labs.com>
2019-04-21 16:03:41 -0500

    added test for 'weird theme'

--- js/components/contexts/ViewContext.test.jsx
+++ js/components/contexts/ViewContext.test.jsx
@@ -19,12 +19,30 @@ const defaultTheme = {
   }
 }
 
-const breakpoints = defaultTheme.breakpoints.keys
-const boundaryTests = Object.entries(defaultTheme.breakpoints.values)
-const subBoundaryTests = boundaryTests.map(([breakpoint, boundary], i) =>
-  i === 0 ? null : [ breakpoints[ i - 1 ], boundary - 1 ]).filter(e => e)
-const superBoundaryTests = boundaryTests.map(([breakpoint, boundary], i) =>
-  [ breakpoints[ i ], boundary + 1])
+const weirdTheme = {
+  breakpoints : {
+    keys : ['foo', 'bar', 'baz'],
+    values : {
+      'foo' : 0,
+      'bar' : 82,
+      'baz' : 105,
+    }
+  }
+}
+
+const generateTestData = (theme) => {
+  const breakpoints = theme.breakpoints.keys
+  const boundaryTests = Object.entries(theme.breakpoints.values)
+  const subBoundaryTests = boundaryTests.map(([breakpoint, boundary], i) =>
+    i === 0 ? null : [ breakpoints[ i - 1 ], boundary - 1 ]).filter(e => e)
+  const superBoundaryTests = boundaryTests.map(([breakpoint, boundary], i) =>
+    [ breakpoints[ i ], boundary + 1])
+
+  return subBoundaryTests.concat(boundaryTests).concat(superBoundaryTests)
+}
+
+const defaultTestData = generateTestData(defaultTheme)
+const weirdTestData = generateTestData(weirdTheme)
 
 const ViewListener = ({callback}) => {
   const viewInfo = useViewInfo()
@@ -33,26 +51,33 @@ const ViewListener = ({callback}) => {
   return <div></div>
 }
 
+const breakpointTestFor = (theme) => (breakpoint, boundary) => {
+  window.innerWidth = boundary
+  let viewInfo
+  const callback = (info) => viewInfo = info
+  render(
+    <ThemeProvider theme={theme}>
+      <ViewContext>
+        <ViewListener callback={callback} />
+      </ViewContext>
+    </ThemeProvider>
+  )
+  expect(viewInfo.breakpoint).toBe(breakpoint)
+}
+
 describe('ViewContext', () => {
   afterEach(() => {
     cleanup()
   })
 
   describe("using default theme", () => {
-    test.each(subBoundaryTests.concat(boundaryTests).concat(superBoundaryTests))
-    ("selects '%s' at boundary %d", (breakpoint, boundary) => {
-      window.innerWidth = boundary
-      let viewInfo
-      const callback = (info) => viewInfo = info
-      render(
-        <ThemeProvider theme={defaultTheme}>
-          <ViewContext>
-            <ViewListener callback={callback} />
-          </ViewContext>
-        </ThemeProvider>
-      )
-      expect(viewInfo.breakpoint).toBe(breakpoint)
-    })
+    test.each(defaultTestData)
+    ("selects '%s' at boundary %d", breakpointTestFor(defaultTheme))
+  })
+
+  describe("using weird theme", () => {
+    test.each(weirdTestData)
+    ("selects '%s' at boundary %d", breakpointTestFor(weirdTheme))
   })
 
   test("does not provide 'x' by default", () => {
